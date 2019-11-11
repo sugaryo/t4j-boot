729f21403db704dc090dc222bbabe62d89085ebc
ryota.sugawara <ryo.sugawara.1224@gmail.com>
2019-11-12 00:25:03 +0900

    first commit

---
+++ t4j-boot/src/main/java/sugaryo/t4jboot/T4jBootApplication.java
@@ -0,0 +1,13 @@
+package sugaryo.t4jboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T4jBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T4jBootApplication.class, args);
+	}
+
+}
---
+++ t4j-boot/src/test/java/sugaryo/t4jboot/T4jBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sugaryo.t4jboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T4jBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
