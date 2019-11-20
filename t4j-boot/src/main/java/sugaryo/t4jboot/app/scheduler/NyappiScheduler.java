b3cf4c9a5c28535c56b7474c42a13d20cf9b935f
sugaryo <ryo.sugawara.1224@gmail.com>
2019-11-21 06:12:42 +0900

    スケジューラにcomponent指定忘れ。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/scheduler/NyappiScheduler.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/scheduler/NyappiScheduler.java
@@ -2,9 +2,11 @@ package sugaryo.t4jboot.app.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import sugaryo.t4jboot.app.module.NyappiCall;
 
+@Component
 public class NyappiScheduler {
 	
 	@Autowired
