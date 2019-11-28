7978622c32c1575d69e8c95f64aa68a65c9f60cf
sugaryo <ryo.sugawara.1224@gmail.com>
2019-11-28 18:28:13 +0900

    不要importの削除。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
@@ -1,8 +1,5 @@
 package sugaryo.t4jboot.app.module;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
