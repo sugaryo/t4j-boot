651df1c84331a6b13e9c6b6a98f576d577641957
sugaryo <ryo.sugawara.1224@gmail.com>
2019-12-03 17:04:10 +0900

    にゃっぴこーるの機能で使用する設定値を外部定義化。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/config/NyappiConfig.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/config/NyappiConfig.java
@@ -12,22 +12,28 @@ public class NyappiConfig {
 	
 	public static final class SelfRetweetConfig {
 		
-		/** セルフリツイートするツイートのID（{@code status.id}）. */
+		/** セルフリツイートするツイートID（{@code status.id}）の配列. */
 		public final long[] ids;
+
+		/** 連続処理するインターバルタイム（{@code Thread.sleep( interval );}）. */
+		public final long interval;
 		
-		private SelfRetweetConfig( String ids ) {
+		private SelfRetweetConfig( String ids, long interval ) {
 			this.ids = JsonMapper.parse( ids, long[].class );
+			this.interval = interval;
 		}
 		
-		private SelfRetweetConfig( long[] ids ) {
+		private SelfRetweetConfig( long[] ids, long interval ) {
 			this.ids = ids;
+			this.interval = interval;
 		}
 	}
 	
 	public final SelfRetweetConfig selfrt;
 	
 	public NyappiConfig(
-			@Value("${nyappi_call.self_rt.ids}") String ids ) {
-		this.selfrt = new SelfRetweetConfig( ids );
+			@Value("${nyappi_call.self_rt.ids}") String selfrtIds,
+			@Value("${nyappi_call.self_rt.interval}") long selfrtInterval ) {
+		this.selfrt = new SelfRetweetConfig( selfrtIds, selfrtInterval );
 	}
 }
--- t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
@@ -1,11 +1,12 @@
 package sugaryo.t4jboot.app.module;
 
+import static sugaryo.t4jboot.common.utility.ThreadUtil.sleep;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import sugaryo.t4jboot.app.api.TwitterApiCall;
 import sugaryo.t4jboot.app.config.ConfigSet;
-import sugaryo.t4jboot.common.utility.ThreadUtil;
 import twitter4j.Status;
 
 @Component
@@ -27,10 +28,12 @@ public class SelfRetweet {
 	
 	public void retweets() {
 		
-		final long[] ids = config.nyappi.selfrt.ids;
+		final long[] ids    = config.nyappi.selfrt.ids;
+		final long interval = config.nyappi.selfrt.interval;
+		
 		for ( final long id : ids ) {
 			this.twitter.retweet( id );
-			ThreadUtil.sleep( 1000 );
+			sleep( interval );
 		}
 	}
 }
