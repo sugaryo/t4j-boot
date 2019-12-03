40da1d95747d56d1fd26183f78178bce06892cb3
sugaryo <ryo.sugawara.1224@gmail.com>
2019-12-03 18:39:06 +0900

    制御乱数の内部構造を改良（連続ミスからの強制ヒット）

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/module/RandomHolder.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/module/RandomHolder.java
@@ -15,21 +15,28 @@ public class RandomHolder {
 	private static final Logger log = LoggerFactory.getLogger( RandomHolder.class );
 	
 	// FIXME：あとでプロパティにしておく。
-	private static final int PREVENT_LEVEL = 3;
-	private static final int HIT_RATIO = 10;
+	private static final int PREVENT_LEVEL = 2;
+	private static final int HIT_RATIO = 50;
+	private static final int MISS_LIMIT = 10;
 	
+	// FIXME：Atomicカウンタが二種類だと間でズレが生じる危険があるので後で修正。
 	private AtomicInteger prevent = new AtomicInteger(0);
+	private AtomicInteger missing = new AtomicInteger(0);
 		
 	private Random random = new Random( new Date().getTime() );
 	
 	public boolean rand() {
-
+		
+		log.debug( "----------------------------------------" );
 		log.debug( "★ prevent-counter : {}", this.prevent.get() );
 		
-		// 抑止中の場合は抑止カウンタをデクリメントしておわり。
+		// ■連続ヒット抑止中の場合：
 		if ( 0 < this.prevent.get() ) {
+			// 抑止中は抑止カウンタをデクリメントしておわり。
+			// ※ 抑止中の強制falseも連続ミスにカウントする。
 			log.info( "◇ 制御乱数[prevent] - 連続ヒットの抑止中。" );
-			this.prevent.decrementAndGet();
+			log.debug( "  - prevent : {}", this.prevent.decrementAndGet() ); // --prevent;
+			log.debug( "  - missing : {}", this.missing.incrementAndGet() ); // ++missing;
 			return false;
 		}
 
@@ -38,15 +45,29 @@ public class RandomHolder {
 		final boolean hit = r % HIT_RATIO == 0;
 		log.debug( "  - random : {}", r );
 
-		// ヒットした場合、抑止カウンタを設定する。
+		// ■ヒットした場合：
 		if ( hit ) {
-			log.info( "◇ 制御乱数[hit] - 連続ヒットの抑止を設定。" );
+			// 抑止カウンタを設定・連続ミスカウンタをリセットしておわり。
+			log.info( "◇ 制御乱数[hit]" );
 			this.prevent.set( PREVENT_LEVEL );
+			this.missing.set( 0 );
+			return true;
 		}
-		// ヒットしなかった場合、なにもしない。
+
+		// ヒットしなかった場合、連続ミスカウンタをインクリメントして連続上限判定。
+		final int miss = this.missing.incrementAndGet();
+		if ( MISS_LIMIT <= miss ) {
+			// ミス限界を超えていた場合は強制ヒット。
+			log.info( "◇ 制御乱数[force-hit]" );
+			this.prevent.set( PREVENT_LEVEL );
+			this.missing.set( 0 );
+			return true;
+		}
+		// ヒットせず、連続ミスカウンタが限界に達していない場合は単なるミスヒット。
 		else {
 			log.info( "◇ 制御乱数[miss]" );
+			log.debug( "  - missing : {}", miss );
+			return false;
 		}
-		return hit;
 	}
 }
