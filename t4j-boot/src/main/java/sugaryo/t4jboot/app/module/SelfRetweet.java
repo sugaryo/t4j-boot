068917101c466eea881871e92b92c610804f8a34
ryota.sugawara <ryo.sugawara.1224@gmail.com>
2020-01-13 16:01:29 +0900

    ログの追加。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/module/SelfRetweet.java
@@ -2,6 +2,8 @@ package sugaryo.t4jboot.app.module;
 
 import static sugaryo.t4jboot.common.utility.ThreadUtil.sleep;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,9 @@ import twitter4j.Status;
 
 @Component
 public class SelfRetweet {
+	
+	private static final Logger log = LoggerFactory.getLogger( SelfRetweet.class );
+	
 	
 	private final TwitterApiCall twitter;
 	private final ConfigSet config;
@@ -23,15 +28,21 @@ public class SelfRetweet {
 	}
 	
 	public Status retweet( final long id ) {
+		
+		log.info( "SelfRetweet retweet by id[{}].", id );
+		
 		return this.twitter.retweet( id );
 	}
 	
 	//TODO：APIのレスポンス用に戻り値を用意したい。
 	public void retweets() {
 		
+		log.info( "SelfRetweet retweets preset all." );
+		
 		final long[] ids    = config.nyappi.selfrt.all();
 		final long interval = config.nyappi.selfrt.interval;
 		
+		log.info( "  - ids.length : {}", ids.length );
 		for ( final long id : ids ) {
 			this.twitter.retweet( id );
 			sleep( interval );
@@ -39,10 +50,13 @@ public class SelfRetweet {
 	}
 	//TODO：APIのレスポンス用に戻り値を用意したい。
 	public void retweets(String category) {
+
+		log.info( "SelfRetweet retweets preset of category[{}].", category );
 		
 		final long[] ids    = config.nyappi.selfrt.of( category );
 		final long interval = config.nyappi.selfrt.interval;
-		
+
+		log.info( "  - ids.length : {}", ids.length );
 		for ( final long id : ids ) {
 			this.twitter.retweet( id );
 			sleep( interval );
