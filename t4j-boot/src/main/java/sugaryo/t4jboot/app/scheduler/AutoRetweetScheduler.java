93d570635a970c79f62a725a33e490b6c9d35582
sugaryo <ryo.sugawara.1224@gmail.com>
2020-02-06 04:18:37 +0900

    時報のにゃっぴコールと処理が被るので、一旦cronを５分に変更。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/scheduler/AutoRetweetScheduler.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/scheduler/AutoRetweetScheduler.java
@@ -12,12 +12,12 @@ public class AutoRetweetScheduler {
 	@Autowired
 	SelfRetweet self;
 
-	@Scheduled(cron = "0 0 9 * 1-5 *")
+	@Scheduled(cron = "0 5 9 * 1-5 *")
 	public void weekday() {
 		// 平日は９時に。
 		this.self.retweets( 10 ); // FIXME：全RTが死ぬほどウザかったので取り敢えず１０件にしとく。
 	}
-	@Scheduled(cron = "0 0 12 * 6-7 *")
+	@Scheduled(cron = "0 5 12 * 6-7 *")
 	public void holiday() {
 		// 土日は１２時に。
 		this.self.retweets( 10 ); // FIXME：全RTが死ぬほどウザかったので取り敢えず１０件にしとく。
