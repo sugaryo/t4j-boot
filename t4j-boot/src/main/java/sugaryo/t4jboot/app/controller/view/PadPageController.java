b8888634c29a6459251fe2b2fa226ae4c8415974
sugaryo <ryo.sugawara.1224@gmail.com>
2021-01-06 21:03:25 +0900

    ルーレット計算ツールを作成。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/controller/view/PadPageController.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/controller/view/PadPageController.java
@@ -1,61 +1,25 @@
 package sugaryo.t4jboot.app.controller.view;
 
-import java.math.BigDecimal;
-import java.util.stream.Stream;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import sugaryo.t4jboot.app.module.MediaTweetCrawller;
-import sugaryo.t4jboot.data.values.MediaTweet;
+import sugaryo.t4jboot.common.constants.pad.DropKind;
+import sugaryo.t4jboot.data.pad.Roulette;
+import sugaryo.t4jboot.data.values.DeciSecScore;
 
 @Controller
 @RequestMapping("nyappi/view/pad")
 public class PadPageController {
 	
 	private static final Logger log = LoggerFactory.getLogger( PadPageController.class );
-	
-	private enum DropKind {
-		
-		/** 回復 */
-		HEART("h", "回復", 0),
-		/** 火 */
-		FIRE("r", "火", 1),
-		/** 水 */
-		WATER("b", "水", 2),
-		/** 木 */
-		WOOD("g", "木", 3),
-		/** 光 */
-		LIGHT("y", "光", 4),
-		/** 闇 */
-		DARK("p", "闇", 5);
-		
-		private final String code;
-		private final String name;
-		private final int index;
-		
-		private DropKind( String code, String name, int index ) {
-			this.code = code;
-			this.name = name;
-			this.index = index;
-		}
-
-
-		public static DropKind from(final String code) {
-			var kinds = DropKind.values();
-			for ( DropKind kind : kinds ) {
-				if ( kind.code.equals( code ) ) return kind;
-			}
-			return null;
-		}
-	}
-	
 	
 	@RequestMapping()
 	public String page( Model model ) {
@@ -66,6 +30,7 @@ public class PadPageController {
 		model.addAttribute( "drop", select );
 		model.addAttribute( "time", 12 );
 		model.addAttribute( "span", 1 );
+		model.addAttribute( "calc", false );
 		
 		return "pad-tool";
 	}
@@ -77,9 +42,6 @@ public class PadPageController {
 			, @RequestParam("roulette_span" ) double span ) {
 
 		var kind = DropKind.from( drop );
-		log.info( "drop:{} ({})", drop, kind.name );
-		log.info( "time:{}", time );
-		log.info( "span:{}", span );
 
 		// 取り敢えずパラメータ伝搬
 		boolean[] select = { false, false, false, false, false, false };
@@ -88,7 +50,88 @@ public class PadPageController {
 		model.addAttribute( "time", time );
 		model.addAttribute( "span", span );
 		
-		// TODO：計算処理
+		// 計算処理
+		var parameter = new Roulette.Parameter( drop, time, span );
+		var puzzle = this.calc( parameter );
+		String[] drops = serialize( puzzle.getDrops() );
+		
+		// 計算結果を設定。
+		model.addAttribute( "calc", true );
+		model.addAttribute( "round", puzzle.getRounds() );
+		model.addAttribute( "remtm", puzzle.getRemTime().seconds() );
+		model.addAttribute( "drops", drops );
+		
 		return "pad-tool";
+	}
+	private Roulette.RoulettePuzzle calc( Roulette.Parameter parameter ) {
+		
+		final int time = parameter.time.score();
+		final int span = parameter.span.score();
+		
+		final int turn = 6 * span; // ６色が一周するのに掛かる時間
+		
+		final int rounds = (int)(time / turn); // ルーレットの回転数 
+		final int last = time % turn; // ルーレットの残り時間 
+		
+		final int remDrop = (int)(last / span); // 最終ターンで進ドロップ数
+		final int remTime = last % span; // 最後に残った時間
+				
+		var drops = build( parameter.drop, rounds, remDrop, 0 != remTime );
+		
+		// 計算結果を返す。
+		var result = new Roulette.RoulettePuzzle();
+		result.setRounds( rounds );
+		result.setRemDrops( remDrop );
+		result.setRemTime( DeciSecScore.of( remTime ) );
+		result.setDrops( drops );
+		
+		log.debug( "roulette-puzzle:【{}】", stringify( drops ) );
+		
+		return result;
+	}
+	
+	private static List<DropKind> build( DropKind drop, int n, int x, boolean rem ) 
+	{		
+		var drops = new ArrayList<DropKind>();
+		
+		// 回転数掛けドロップ数
+		// 最終ターンで進むドロップ数
+		// あまり時間があればプラス１
+		int total = (6 * n) + x + (rem ? 1 : 0);
+		
+		// 基準ドロップを設定。
+		DropKind d = drop;
+		
+		// 基準ドロップから逆順でルーレット遷移させる。
+		for ( int i = 0; i < total; i++ ) {
+			drops.add( d );
+			d = d.prev();
+		}
+		
+		// 最終的な目的ドロップから逆順で詰めたので、リストをリバースして返す。
+		Collections.reverse( drops );
+		return drops;
+	}
+	
+	private static String stringify( List<DropKind> drops ) {
+		var sb = new StringBuilder();
+		
+		int i = 0;
+		String separator = "";
+		for ( DropKind drop : drops ) {
+			if ( i++ % 6 == 0 ) {
+				sb.append( separator );
+				separator = " ";
+			}
+			sb.append( drop.name );
+		}
+		
+		return sb.toString();
+	}
+	
+	private static String[] serialize( List<DropKind> drops ) {
+		return drops.stream()
+				.map( x -> x.code )
+				.toArray( String[]::new );
 	}
 }
