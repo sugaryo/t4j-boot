a2e08a9d51c7e17cf16fd7af6f15d73037e858e2
sugaryo <ryo.sugawara.1224@gmail.com>
2022-07-28 13:18:16 +0900

    DisplayNameを変更するエンドポイントを実装。

--- t4j-boot/src/main/java/sugaryo/t4jboot/app/controller/rest/NyappiController.java
+++ t4j-boot/src/main/java/sugaryo/t4jboot/app/controller/rest/NyappiController.java
@@ -3,12 +3,14 @@ package sugaryo.t4jboot.app.controller.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sugaryo.t4jboot.app.config.ConfigSet;
 import sugaryo.t4jboot.app.controller.rest.strategy.ResponseParameterStrategy;
+import sugaryo.t4jboot.app.module.DisplayNameChanger;
 import sugaryo.t4jboot.app.module.NyappiCall;
 
 
@@ -26,6 +28,9 @@ public class NyappiController {
 	@Autowired
 	NyappiCall nyappi;
 	
+	@Autowired
+	DisplayNameChanger displayname;
+	
 	//FIXME : NyyappiCall モジュールの方が戻り値対応出来てないので、まだ使えない。
 	@Autowired
 	ResponseParameterStrategy response;
@@ -35,5 +40,16 @@ public class NyappiController {
 	public void nyappi() {
 		this.nyappi.call();
 	}
+	
+	
+	@PostMapping("display-name/shuffle")
+	public String shuffleDisplayName() {
+		return this.displayname.shuffle();
+	}
+	@PostMapping("display-name/change/{code}")
+	public String changeDisplayName(@PathVariable int code) {
+		return this.displayname.change(code);
+	}
+	
 	
 }
